8f3dc4601e2ada3a0b1b5f99388325b0c757cd05
Rasmus <rasmus.hellstrom@ha.ax>
2023-12-03 10:40:47 +0200

    Updated to Axum 0.7 and accomodated breaking changes

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,6 @@ async fn main() {
     );
 
     let addr = SocketAddr::from(([0, 0, 0, 0], args.port));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
